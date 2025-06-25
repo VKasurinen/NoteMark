7cd9963feaa90192a32ac90f7e78e276488f05f0
vkasurinen <vaino.kasurinen@hotmail.fi>
2025-06-25 21:58:15 +0300

    Small fix

--- app/src/main/java/com/vkasurinen/notemark/notes/presentation/notes_details/DetailEvent.kt
+++ app/src/main/java/com/vkasurinen/notemark/notes/presentation/notes_details/DetailEvent.kt
@@ -1,7 +1,6 @@
 package com.vkasurinen.notemark.notes.presentation.notes_details
 
-sealed class DetailEvent {
-    data object NavigateToNotes : DetailEvent()
-    data class ShowValidationError(val message: String) : DetailEvent()
-
+sealed interface DetailEvent{
+    data object NavigateToNotes : DetailEvent
+    data class ShowValidationError(val message: String) : DetailEvent
 }
