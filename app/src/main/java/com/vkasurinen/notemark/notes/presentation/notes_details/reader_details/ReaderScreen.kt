10383e20d263bdcd87e03b7338abb40efb0c56de
vkasurinen <vaino.kasurinen@hotmail.fi>
2025-07-04 20:53:07 +0300

    Reader screen update

--- app/src/main/java/com/vkasurinen/notemark/notes/presentation/notes_details/reader_details/ReaderScreen.kt
+++ app/src/main/java/com/vkasurinen/notemark/notes/presentation/notes_details/reader_details/ReaderScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -30,6 +31,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -72,12 +74,12 @@ fun ReaderScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 10.dp, bottom = 30.dp)
+                .padding(top = 10.dp)
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 18.dp, vertical = 4.dp)
+                    .padding(horizontal = 18.dp, vertical = 2.dp)
             ) {
                 Box(
                     modifier = Modifier
@@ -110,107 +112,118 @@ fun ReaderScreen(
             }
 
 
-            Spacer(modifier = Modifier.height(18.dp))
-
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .weight(1f),
-                contentAlignment = Alignment.Center
+                    .padding(horizontal = 20.dp)
+                    .weight(1f)
             ) {
-                Column(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .verticalScroll(rememberScrollState()),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Text(
-                        text = state.title,
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = MaterialTheme.colorScheme.onSurface,
-                            fontFamily = SpaceGrotesk
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    HorizontalDivider(
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .alpha(0.5f),
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly
+                            .padding(horizontal = 16.dp)
+                            .verticalScroll(rememberScrollState()),
                     ) {
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(
-                                text = "Date created",
-                                style = MaterialTheme.typography.labelLarge.copy(
-                                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                                )
+                        Text(
+                            text = state.title,
+                            modifier = Modifier.fillMaxWidth(0.7f),
+                            style = TextStyle(
+                                textAlign = TextAlign.Start,
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.onSurface,
+                                fontFamily = SpaceGrotesk
                             )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = formatDate(state.createdAt, isCreationDate = true),
-                                style = MaterialTheme.typography.labelLarge.copy(
-                                    color = MaterialTheme.colorScheme.onSurface,
-                                    fontFamily = SpaceGrotesk
-                                )
-                            )
-                        }
+                        )
 
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(
-                                text = "Last edited",
-                                style = MaterialTheme.typography.labelLarge.copy(
-                                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                            Text(
-                                text = formatDate(state.lastEditedAt),
-                                style = MaterialTheme.typography.labelLarge.copy(
-                                    color = MaterialTheme.colorScheme.onSurface,
-                                    fontFamily = SpaceGrotesk
-                                )
-                            )
-                        }
-                    }
 
-                    Spacer(modifier = Modifier.height(16.dp))
+                        Spacer(modifier = Modifier.height(16.dp))
 
-                    HorizontalDivider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .alpha(0.5f),
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Text(
-                        text = state.content,
-                        style = TextStyle(
-                            fontSize = 16.sp,
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .fillMaxWidth(0.7f)
+                                .align(Alignment.CenterHorizontally)
+                                .alpha(0.5f),
                             color = MaterialTheme.colorScheme.onSurfaceVariant
                         )
-                    )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Row(
+                            modifier = Modifier
+                                .wrapContentWidth()
+                                .align(Alignment.Start),
+                            horizontalArrangement = Arrangement.spacedBy(32.dp)
+                        ) {
+                            Column {
+                                Text(
+                                    text = "Date created",
+                                    style = MaterialTheme.typography.labelLarge.copy(
+                                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                                    )
+                                )
+                                Spacer(modifier = Modifier.height(4.dp))
+                                Text(
+                                    text = formatDate(state.createdAt, isCreationDate = true),
+                                    style = MaterialTheme.typography.labelLarge.copy(
+                                        color = MaterialTheme.colorScheme.onSurface,
+                                        fontFamily = SpaceGrotesk
+                                    )
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.width(60.dp))
+
+
+                            Column {
+                                Text(
+                                    text = "Last edited",
+                                    style = MaterialTheme.typography.labelLarge.copy(
+                                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                                    )
+                                )
+                                Spacer(modifier = Modifier.height(4.dp))
+                                Text(
+                                    text = formatDate(state.lastEditedAt),
+                                    style = MaterialTheme.typography.labelLarge.copy(
+                                        color = MaterialTheme.colorScheme.onSurface,
+                                        fontFamily = SpaceGrotesk
+                                    )
+                                )
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .fillMaxWidth(0.7f)
+                                .align(Alignment.CenterHorizontally)
+                                .alpha(0.5f),
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text(
+                            text = state.content,
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        )
+                    }
                 }
             }
         }
 
-        // ModeSwitcher at the bottom
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(bottom = 50.dp),
+                .padding(bottom = 20.dp),
             contentAlignment = Alignment.BottomCenter
         ) {
             ModeSwitcher(
@@ -228,7 +241,6 @@ fun ReaderScreen(
 }
 
 
-
 @Preview(
     showBackground = true,
     widthDp = 800,
@@ -240,7 +252,11 @@ fun ReaderScreen(
 private fun Preview() {
     NoteMarkTheme {
         ReaderScreen(
-            state = ReaderState(),
+            state = ReaderState(
+                createdAt = "12.4.2025",
+                lastEditedAt = "Just now",
+                content = "asdasdaddsadsasdadsasdasddasdasasd"
+            ),
             onAction = {}
         )
     }
